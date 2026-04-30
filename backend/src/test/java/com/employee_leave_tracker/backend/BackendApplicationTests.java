20f6fa566c075c4bb0496a5d60ead7ee605b98d8
Ashiqur Rahman <ashiq.edu.bd@gmail.com>
2026-04-30 21:07:52 +0600

    feat: project setup
    - added Spring Boot backend structure

---
+++ backend/src/test/java/com/employee_leave_tracker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_leave_tracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
